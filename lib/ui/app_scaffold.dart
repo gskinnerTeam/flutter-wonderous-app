f2010f27452249ddbf51b532a0bef147a93de0d7
Shawn <shawn@gskinner.com>
2022-03-16 02:14:38 -0600

    v0.0.2+3

--- lib/ui/app_scaffold.dart
+++ lib/ui/app_scaffold.dart
@@ -1,5 +1,6 @@
 import 'package:wonders/common_libs.dart';
 import 'package:wonders/ui/common/app_scroll_behavior.dart';
+import 'package:statsfl/statsfl.dart';
 
 class WondersAppScaffold extends StatelessWidget with GetItMixin {
   WondersAppScaffold({Key? key, required this.child}) : super(key: key);
@@ -13,22 +14,24 @@ class WondersAppScaffold extends StatelessWidget with GetItMixin {
     // Construct an AppStyle using app size and current themeType.
     final styles = AppStyle(screenSize: size, colors: theme);
     // Pass the style down to the tree with provider and inject a themData to style existing Material components.
-    return Theme(
-      data: theme.toThemeData(),
-      child: Provider<AppStyle>.value(
-        value: styles,
-        builder: (context, child) => child!,
-        // Provide a custom scroll config for easier responsive testing on desktop
-        child: ScrollConfiguration(
-          behavior: AppScrollBehavior(),
-          // Provide a default texts style to allow Hero's to render text properly
-          child: DefaultTextStyle(
-            style: styles.text.body,
-            child: Column(
-              children: [
-                if (showUrl) Text(appRouter.location),
-                Expanded(child: child),
-              ],
+    return StatsFl(
+      child: Theme(
+        data: theme.toThemeData(),
+        child: Provider<AppStyle>.value(
+          value: styles,
+          builder: (context, child) => child!,
+          // Provide a custom scroll config for easier responsive testing on desktop
+          child: ScrollConfiguration(
+            behavior: AppScrollBehavior(),
+            // Provide a default texts style to allow Hero's to render text properly
+            child: DefaultTextStyle(
+              style: styles.text.body,
+              child: Column(
+                children: [
+                  if (showUrl) Text(appRouter.location),
+                  Expanded(child: child),
+                ],
+              ),
             ),
           ),
         ),
