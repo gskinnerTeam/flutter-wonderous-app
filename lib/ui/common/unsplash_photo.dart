880aed259dbe5a7d1b1ca2f4cb606037eb773558
Shawn <shawn@gskinner.com>
2022-04-01 12:08:06 -0600

    Hide unsplash credits by default

--- lib/ui/common/unsplash_photo.dart
+++ lib/ui/common/unsplash_photo.dart
@@ -4,7 +4,7 @@ import 'package:wonders/logic/data/unsplash_photo_data.dart';
 import 'package:wonders/ui/common/controls/app_loader.dart';
 
 class UnsplashPhoto extends StatelessWidget {
-  const UnsplashPhoto(this.id, {Key? key, this.fit = BoxFit.cover, required this.targetSize, this.showCredits = true})
+  const UnsplashPhoto(this.id, {Key? key, this.fit = BoxFit.cover, required this.targetSize, this.showCredits = false})
       : super(key: key);
   final String id;
   final BoxFit fit;
@@ -24,20 +24,24 @@ class UnsplashPhoto extends StatelessWidget {
                   fit: StackFit.expand,
                   children: [
                     CachedNetworkImage(imageUrl: url, fit: fit, placeholder: (_, __) => Center(child: AppLoader())),
-                    BottomCenter(
-                      child: AnimatedOpacity(
-                        opacity: showCredits ? 1 : 0,
-                        duration: context.times.fast,
-                        child: Container(
-                          width: double.infinity,
-                          color: Colors.black.withOpacity(.5),
-                          padding: EdgeInsets.all(context.insets.xs),
-                          //TODO: Add proper credits and apply for unsplash
-                          child: Text('Photo by ${snapshot.data} on Unsplash',
-                              style: context.style.text.caption.copyWith(color: context.colors.text)),
+                    if (showCredits) ...[
+                      BottomCenter(
+                        child: AnimatedOpacity(
+                          opacity: showCredits ? 1 : 0,
+                          duration: context.times.fast,
+                          child: Container(
+                            width: double.infinity,
+                            color: Colors.black.withOpacity(.5),
+                            padding: EdgeInsets.all(context.insets.xs),
+                            //TODO: Add proper credits and apply for unsplash
+                            child: Text(
+                              'Photo by Nikolai Tesla on Unsplash',
+                              style: context.style.text.caption.copyWith(color: context.colors.text),
+                            ),
+                          ),
                         ),
-                      ),
-                    )
+                      )
+                    ]
                   ],
                 );
         });
