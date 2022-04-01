e61bceab7c10ffdda9d72f80d2f72b571859652c
Shawn <shawn@shawnblais.com>
2022-04-01 13:45:13 -0600

    Add tweaks for smaller screens

--- lib/ui/screens/wonder_details/editorial/wonder_editorial_screen.dart
+++ lib/ui/screens/wonder_details/editorial/wonder_editorial_screen.dart
@@ -11,11 +11,11 @@ import 'package:wonders/ui/common/unsplash_photo.dart';
 import 'package:wonders/ui/wonder_illustrations/wonder_illustration.dart';
 import 'package:wonders/ui/wonder_illustrations/wonder_illustration_config.dart';
 
-part 'editorial_title.dart';
+part 'circular_title_bar.dart';
+part 'editorial_app_bar.dart';
 part 'editorial_content.dart';
 part 'editorial_illustration.dart';
-part 'circular_title_bar.dart';
-part 'collapsing_app_bar.dart';
+part 'editorial_title.dart';
 
 class WonderEditorialScreen extends StatefulWidget {
   const WonderEditorialScreen(this.data, {Key? key}) : super(key: key);
@@ -27,12 +27,6 @@ class WonderEditorialScreen extends StatefulWidget {
 
 class _WonderEditorialScreenState extends State<WonderEditorialScreen> {
   late final ScrollController _scroller = ScrollController()..addListener(_handleScrollChanged);
-
-  /// TODO: Make these final once design has stabilized
-  double get _headerHeight => 330;
-  double get _minAppBarHeight => 190;
-  double get _maxAppBarHeight => 700;
-
   final _scrollPos = ValueNotifier(0.0);
 
   @override
@@ -51,84 +45,94 @@ class _WonderEditorialScreenState extends State<WonderEditorialScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return ColoredBox(
-      color: context.colors.wonderBg(widget.data.type),
-      child: Stack(
-        children: [
-          /// Illustration - Sits underneath the scrolling content, fades out as it scrolls
-          SizedBox(
-            height: _headerHeight,
-            child: ValueListenableBuilder<double>(
-              valueListenable: _scrollPos,
-              builder: (_, value, child) {
-                // get some value between 0 and 1, based on the amt scrolled
-                double opacity = max(0, 1 - value / 500);
-                return Opacity(opacity: opacity, child: child);
-              },
-              child: _TopIllustration(widget.data.type),
-            ),
-          ),
+    return LayoutBuilder(builder: (_, constraints) {
+      bool shortMode = constraints.biggest.height < 700;
+      double _headerHeight = shortMode ? 250 : 330;
+      double _minAppBarHeight = shortMode ? 40 : 190;
+      double _maxAppBarHeight = shortMode ? 500 : 700;
 
-          /// Scrolling content - Includes an invisible gap at the top, and then scrolls over the illustration
-          CustomScrollView(
-            primary: false,
-            controller: _scroller,
-            slivers: [
-              /// Invisible padding at the top of the list, so the illustration shows through the btm
-              SliverToBoxAdapter(
-                child: EightWaySwipeDetector(
-                  onSwipe: _handleListTopSwipe,
-                  child: SizedBox(height: _headerHeight + context.insets.md),
-                ),
+      return ColoredBox(
+        color: context.colors.wonderBg(widget.data.type),
+        child: Stack(
+          children: [
+            /// Illustration - Sits underneath the scrolling content, fades out as it scrolls
+            SizedBox(
+              height: _headerHeight,
+              child: ValueListenableBuilder<double>(
+                valueListenable: _scrollPos,
+                builder: (_, value, child) {
+                  // get some value between 0 and 1, based on the amt scrolled
+                  double opacity = (1 - value / 500).clamp(0, 1);
+                  return Opacity(opacity: opacity, child: child);
+                },
+                child: _TopIllustration(widget.data.type),
               ),
+            ),
 
-              /// Text content, animates itself to hide behind the app bar as it scrolls up
-              SliverToBoxAdapter(
-                  child: ValueListenableBuilder<double>(
-                      valueListenable: _scrollPos,
-                      builder: (_, value, child) {
-                        double offsetAmt = max(0, value * .3);
-                        double opacity = max(0, 1 - offsetAmt / 150);
-                        return Transform.translate(
-                          offset: Offset(0, offsetAmt),
-                          child: Opacity(opacity: opacity, child: child),
-                        );
-                      },
-                      child: _TitleText(widget.data))),
-
-              /// App bar
-              SliverAppBar(
-                pinned: true,
-                collapsedHeight: _minAppBarHeight,
-                expandedHeight: _maxAppBarHeight,
-                backgroundColor: Colors.transparent,
-                leading: SizedBox.shrink(),
-                flexibleSpace: ValueListenableBuilder<double>(
-                  valueListenable: _scrollPos,
-                  builder: (_, value, child) {
-                    double opacity = min(1, .4 + (value / 1000));
-                    return Opacity(opacity: Curves.easeIn.transform(opacity), child: child);
-                  },
-                  child: SizedBox.expand(
-                    child: _CollapsingAppBar(widget.data.type, imageId: 'VPavA7BBxK0'),
+            /// Scrolling content - Includes an invisible gap at the top, and then scrolls over the illustration
+            CustomScrollView(
+              primary: false,
+              controller: _scroller,
+              physics: ClampingScrollPhysics(),
+              slivers: [
+                /// Invisible padding at the top of the list, so the illustration shows through the btm
+                SliverToBoxAdapter(
+                  child: EightWaySwipeDetector(
+                    onSwipe: _handleListTopSwipe,
+                    child: SizedBox(height: _headerHeight + context.insets.md),
                   ),
                 ),
-                //ColoredBox(color: Colors.grey.shade200, child: Placeholder())
-              ),
 
-              /// Editorial content (text and images)
-              SliverToBoxAdapter(
-                child: _EditorialContent(widget.data),
-              ),
+                /// Text content, animates itself to hide behind the app bar as it scrolls up
+                SliverToBoxAdapter(
+                    child: ValueListenableBuilder<double>(
+                        valueListenable: _scrollPos,
+                        builder: (_, value, child) {
+                          double offsetAmt = max(0, value * .3);
+                          double opacity = (1 - offsetAmt / 150).clamp(0, 1);
+                          ;
+                          return Transform.translate(
+                            offset: Offset(0, offsetAmt),
+                            child: Opacity(opacity: opacity, child: child),
+                          );
+                        },
+                        child: _TitleText(widget.data))),
 
-              /// Bottom padding
-              SliverToBoxAdapter(
-                child: Container(height: 100, color: context.colors.bg),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
+                /// App bar
+                SliverAppBar(
+                  pinned: true,
+                  collapsedHeight: _minAppBarHeight,
+                  toolbarHeight: _minAppBarHeight,
+                  expandedHeight: _maxAppBarHeight,
+                  backgroundColor: Colors.transparent,
+                  leading: SizedBox.shrink(),
+                  flexibleSpace: ValueListenableBuilder<double>(
+                    valueListenable: _scrollPos,
+                    builder: (_, value, child) {
+                      double opacity = (.4 + (value / 1000)).clamp(0, 1);
+                      return Opacity(opacity: Curves.easeIn.transform(opacity), child: child);
+                    },
+                    child: SizedBox.expand(
+                      child: _EditorialAppBar(widget.data.type, imageId: 'VPavA7BBxK0'),
+                    ),
+                  ),
+                  //ColoredBox(color: Colors.grey.shade200, child: Placeholder())
+                ),
+
+                /// Editorial content (text and images)
+                SliverToBoxAdapter(
+                  child: _EditorialContent(widget.data),
+                ),
+
+                /// Bottom padding
+                SliverToBoxAdapter(
+                  child: Container(height: 100, color: context.colors.bg),
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+    });
   }
 }
