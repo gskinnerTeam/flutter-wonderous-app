759c0a41c1e097a62bc69d365038f2d1a0e86dfe
Alex <alex@gskinner.com>
2022-04-08 09:41:18 -0600

    Artifact Highlight: Brightened the background image a bit.

--- lib/ui/screens/artifact/artifact_highlights/artifact_blurred_bg.dart
+++ lib/ui/screens/artifact/artifact_highlights/artifact_blurred_bg.dart
@@ -10,6 +10,8 @@ class ArtifactBlurredBg extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Color gradientColor = context.colors.greyStrong.withAlpha(155);
+
     if (url.isEmpty) {
       return Container(
         color: context.colors.greyStrong,
@@ -25,17 +27,17 @@ class ArtifactBlurredBg extends StatelessWidget {
       ),
       child: VtGradient(
         // Vertical gradient from top down.
-        [context.colors.greyStrong, context.colors.greyStrong, Colors.white],
-        const [0.0, 0.2, 1.0],
+        [gradientColor, gradientColor, Colors.white, Colors.white],
+        const [0.0, 0.1, 0.8, 1.0],
         blendMode: BlendMode.multiply,
         child: HzGradient(
           // Horizontal gradient on edges of screen.
-          [context.colors.greyStrong, Colors.white, Colors.white, context.colors.greyStrong],
+          [gradientColor, Colors.white, Colors.white, gradientColor],
           const [0.0, 0.05, 0.95, 1.0],
           blendMode: BlendMode.multiply,
           child: BackdropFilter(
             // Blur effect
-            filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+            filter: ImageFilter.blur(sigmaX: 6, sigmaY: 6),
             child: Container(
               decoration: BoxDecoration(
                 color: Colors.white.withOpacity(0.0),
