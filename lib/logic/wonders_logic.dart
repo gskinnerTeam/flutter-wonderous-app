e63ed43622a7c79bfc954c7044c327356aa787b3
Grant Skinner <grant@gskinner.com>
2022-04-18 10:58:04 -0600

    Add all wonders to WonderLogic.

--- lib/logic/wonders_logic.dart
+++ lib/logic/wonders_logic.dart
@@ -1,11 +1,25 @@
 import 'package:wonders/common_libs.dart';
 import 'package:wonders/logic/data/wonder_data.dart';
 import 'package:wonders/logic/data/wonders_data/chichen_itza_data.dart';
+import 'package:wonders/logic/data/wonders_data/christ_redeemer_data.dart';
+import 'package:wonders/logic/data/wonders_data/colosseum_data.dart';
+import 'package:wonders/logic/data/wonders_data/great_wall_data.dart';
 import 'package:wonders/logic/data/wonders_data/machu_picchu_data.dart';
+import 'package:wonders/logic/data/wonders_data/petra_data.dart';
+import 'package:wonders/logic/data/wonders_data/pyramids_giza_data.dart';
 import 'package:wonders/logic/data/wonders_data/taj_mahal_data.dart';
 
 class WondersLogic {
-  ValueNotifier<List<WonderData>> all = ValueNotifier([chichenItzaData, tajMahalData, colosseumData]);
+  ValueNotifier<List<WonderData>> all = ValueNotifier([
+    chichenItzaData,
+    christRedeemerData,
+    colosseumData,
+    greatWallData,
+    machuPicchuData,
+    petraData,
+    pyramidsGizaData,
+    tajMahalData,
+  ]);
 
   WonderData getDataForType(WonderType value) {
     WonderData? result = all.value.firstWhereOrNull((w) => w.type == value);
