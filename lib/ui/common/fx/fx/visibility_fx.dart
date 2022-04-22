aac8d5c38e233a9574af42a9f8e012e323cd4383
Grant Skinner <grant@gskinner.com>
2022-04-22 10:06:27 -0600

    Add alignment to ScaleFX, tweaks to VisibilityFX

--- lib/ui/common/fx/fx/visibility_fx.dart
+++ lib/ui/common/fx/fx/visibility_fx.dart
@@ -3,30 +3,29 @@ import 'package:flutter/widgets.dart';
 import '../fx.dart';
 
 // todo: possibly add other properties of visibility as params?
+// todo: also maybe a `maintain` param that shortcuts all of them
 
 @immutable
 class VisibilityFX extends AbstractFX<bool> {
-  const VisibilityFX({Duration? delay, bool begin = false})
-      : super(delay: delay, duration: Duration.zero, begin: begin);
+  const VisibilityFX({Duration? delay, bool end = true})
+      : super(delay: delay, duration: Duration.zero, begin: !end, end: end,);
 
   @override
   Widget build(BuildContext context, Widget child, AnimationController controller, FXEntry entry) {
     // instead of setting up an animation, we can optimize a bit to calculate the callback time once:
     double ratio = entry.begin.inMilliseconds / (controller.duration?.inMilliseconds ?? 0);
     bool visible = begin! == (controller.value < ratio);
-    return visible
-        ? child
-        : Visibility(
-            child: child,
-            visible: false,
-            maintainSize: true,
-            maintainAnimation: true,
-            maintainState: true,
-          );
+    return Visibility(
+      child: child,
+      visible: visible,
+      maintainSize: true,
+      maintainAnimation: true,
+      maintainState: true,
+    );
   }
 }
 
 extension CallbackFXExtensions<T> on FXManager<T> {
-  T show({Duration? delay}) => addFX(VisibilityFX(delay: delay, begin: false));
-  T hide({Duration? delay}) => addFX(VisibilityFX(delay: delay, begin: true));
+  T show({Duration? delay}) => addFX(VisibilityFX(delay: delay, end: true));
+  T hide({Duration? delay}) => addFX(VisibilityFX(delay: delay, end: false));
 }
