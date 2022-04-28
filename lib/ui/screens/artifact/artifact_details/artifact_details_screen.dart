a867876559af7d2f475b97d6631edfa2a39b477b
Alex <alex@gskinner.com>
2022-04-28 17:42:20 -0600

    Added styled preloader to ArtifactDetails screen.

--- lib/ui/screens/artifact/artifact_details/artifact_details_screen.dart
+++ lib/ui/screens/artifact/artifact_details/artifact_details_screen.dart
@@ -28,12 +28,35 @@ class _ArtifactDetailsScreenState extends State<ArtifactDetailsScreen> {
 
   @override
   Widget build(BuildContext context) {
+    var preloadScreen = Stack(
+      children: [
+        BackBtn().safe(),
+        Center(
+            child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 50,
+              height: 50,
+              child: AppLoader(),
+            ),
+            Gap(context.insets.md),
+            Text(
+              'Just a moment, please...',
+              style: context.textStyles.body.copyWith(color: context.colors.accent1),
+            ),
+          ],
+        )),
+      ],
+    );
+
     return ColoredBox(
       color: context.colors.greyStrong,
       child: FutureBuilder<ArtifactData?>(
         future: _future,
         builder: (_, snapshot) {
-          if (snapshot.hasData == false) return Center(child: AppLoader());
+          if (snapshot.hasData == false) return preloadScreen;
           final data = snapshot.data;
           if (data == null) {
             return AppLoadError(label: 'Unable to find info for artifact ${widget.artifactId} ');
