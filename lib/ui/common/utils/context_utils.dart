b9cdd0e2c5060fcaf1bf8337da6fbd76df1beddf
Shawn <shawn@gskinner.com>
2022-04-27 22:37:15 -0600

    fix potential errors in ContextUtils

--- lib/ui/common/utils/context_utils.dart
+++ lib/ui/common/utils/context_utils.dart
@@ -3,11 +3,17 @@ import 'package:flutter/material.dart';
 class ContextUtils {
   static Offset? getGlobalPos(BuildContext context, [Offset offset = Offset.zero]) {
     final rb = context.findRenderObject() as RenderBox?;
-    return rb?.localToGlobal(offset);
+    if (rb?.hasSize == true) {
+      return rb?.localToGlobal(offset);
+    }
+    return null;
   }
 
   static Size? getSize(BuildContext context) {
     final rb = context.findRenderObject() as RenderBox?;
-    return rb?.size;
+    if (rb?.hasSize == true) {
+      return rb?.size;
+    }
+    return null;
   }
 }
