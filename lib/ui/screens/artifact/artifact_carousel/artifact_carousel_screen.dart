a2321000852b238acb464d4684121399a168942f
Shawn <shawn@gskinner.com>
2022-05-02 10:07:08 -0600

    cleanup

--- lib/ui/screens/artifact/artifact_carousel/artifact_carousel_screen.dart
+++ lib/ui/screens/artifact/artifact_carousel/artifact_carousel_screen.dart
@@ -3,6 +3,7 @@ import 'dart:math' as math;
 
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:wonders/common_libs.dart';
+import 'package:wonders/logic/data/artifact_data.dart';
 import 'package:wonders/logic/data/highlights_data.dart';
 import 'package:wonders/ui/common/controls/app_loader.dart';
 import 'package:wonders/ui/common/gradient_container.dart';
@@ -29,7 +30,7 @@ class _ArtifactScreenState extends State<ArtifactCarouselScreen> {
   static const double _maxElementWidth = 400;
   // Used to determine white background dimensions.
   static const double _maxBottomHeight = 300;
-  final _loadedArtifacts = [];
+  final _loadedArtifacts = <HighlightsData>[];
   late PageController _controller;
   HighlightsData? _currentArtifact;
   double _currentPage = 0;
@@ -39,13 +40,12 @@ class _ArtifactScreenState extends State<ArtifactCarouselScreen> {
     super.initState();
 
     _currentPage = _highlightedArtifactIds.length * 5000;
-    _controller =
-        PageController(initialPage: _currentPage.toInt(), viewportFraction: _pageViewportFraction, keepPage: true);
-    _controller.addListener(() {
-      setState(() {
-        _currentPage = _controller.page ?? 0.0;
-      });
-    });
+    _controller = PageController(
+      initialPage: _currentPage.toInt(),
+      viewportFraction: _pageViewportFraction,
+      keepPage: true,
+    );
+    _controller.addListener(_handlePageChanged);
 
     _loadedArtifacts.clear();
     _loadedArtifacts.addAll(HighlightsData.forWonder(widget.type));
@@ -55,20 +55,19 @@ class _ArtifactScreenState extends State<ArtifactCarouselScreen> {
     } else {
       debugPrint('ERROR: No artifacts found for ${widget.type}');
     }
+  }
 
-    // Update the screen.
-    setState(() {});
+  void _handlePageChanged() {
+    setState(() => _currentPage = _controller.page ?? 0.0);
   }
 
   @override
   void dispose() {
-    // Ensure the contorller is disposed of properly.
     _controller.dispose();
     super.dispose();
   }
 
   void _changeArtifactIndex(int index) {
-    //_controller.jumpToPage(index);
     setState(() {
       _currentArtifact = _loadedArtifacts[index % _highlightedArtifactIds.length];
     });
@@ -76,8 +75,7 @@ class _ArtifactScreenState extends State<ArtifactCarouselScreen> {
 
   Future<void> _animatePageJump(int to) async {
     scheduleMicrotask(() {
-      int ms = 300;
-      _controller.animateToPage(to, duration: Duration(milliseconds: ms), curve: Curves.easeOut);
+      _controller.animateToPage(to, duration: context.times.fast, curve: Curves.easeOut);
     });
   }
 
@@ -85,8 +83,10 @@ class _ArtifactScreenState extends State<ArtifactCarouselScreen> {
     _animatePageJump(_controller.page!.round() + jumpBy);
   }
 
-  void _handleArtifactTap(int index) =>
-      context.push(ScreenPaths.artifact(_loadedArtifacts[index % _loadedArtifacts.length].artifactId.toString()));
+  void _handleArtifactTap(int index) {
+    HighlightsData data = _loadedArtifacts[index % _loadedArtifacts.length];
+    context.push(ScreenPaths.artifact(data.artifactId));
+  }
 
   void _handleSearchButtonTap() => context.push(ScreenPaths.search(widget.type));
 
@@ -118,9 +118,11 @@ class _ArtifactScreenState extends State<ArtifactCarouselScreen> {
         children: [
           // Background Image
           AnimatedSwitcher(
-            child:
-                ArtifactCarouselBg(key: ValueKey(_currentArtifact?.artifactId), url: _currentArtifact?.imageUrl ?? ''),
-            duration: Duration(milliseconds: 300),
+            child: ArtifactCarouselBg(
+              key: ValueKey(_currentArtifact?.artifactId),
+              url: _currentArtifact?.imageUrl ?? '',
+            ),
+            duration: context.times.fast,
           ),
 
           // White space, covering bottom half.
@@ -130,8 +132,9 @@ class _ArtifactScreenState extends State<ArtifactCarouselScreen> {
               height: _bottomHeight + math.min(context.widthPx, _maxBottomHeight) / 2,
               decoration: BoxDecoration(
                 color: context.colors.offWhite,
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(_maxElementWidth / 2), topRight: Radius.circular(_maxElementWidth / 2)),
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(_maxElementWidth / 2),
+                ),
               ),
             ),
           ),
@@ -176,7 +179,6 @@ class _ArtifactScreenState extends State<ArtifactCarouselScreen> {
             ),
           ),
 
-          // Text Content
           BottomCenter(
             child: Padding(
               padding: EdgeInsets.symmetric(horizontal: context.insets.md),
@@ -184,68 +186,14 @@ class _ArtifactScreenState extends State<ArtifactCarouselScreen> {
                 mainAxisSize: MainAxisSize.min,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  // Title and Desc
                   Gap(context.insets.md),
-                  IgnorePointer(
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        // Wonder Name
-                        Text(
-                          (_loadedArtifacts.isEmpty ? 'Just a moment...' : _currentArtifact?.culture ?? '')
-                              .toUpperCase(),
-                          style: context.textStyles.titleFont.copyWith(
-                            color: context.colors.accent1,
-                            fontSize: 14,
-                            height: 1.2,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                        Gap(context.insets.md),
-
-                        _loadedArtifacts.isEmpty
-                            ? AppLoader()
-                            : FXAnimate(
-                                fx: const [FadeFX()],
-                                key: ValueKey(_currentArtifact?.artifactId),
-                                child: Column(
-                                  children: [
-                                    // Artifact Title
-                                    Text(
-                                      _currentArtifact?.title ?? '',
-                                      style: context.textStyles.h2.copyWith(color: context.colors.greyStrong),
-                                      textAlign: TextAlign.center,
-                                      maxLines: 1,
-                                    ),
-                                    Gap(context.insets.xs),
-
-                                    // Time frame
-                                    Text(
-                                      _currentArtifact?.date ?? '',
-                                      style: context.textStyles.body.copyWith(color: context.colors.body),
-                                      textAlign: TextAlign.center,
-                                    ),
-                                  ],
-                                ),
-                              ),
-
-                        Gap(context.insets.lg),
-                        // Selection indicator
-                        _buildPageIndicator(context, _highlightedArtifactIds.length),
-                      ],
-                    ),
-                  ),
-
+                  // Text Content
+                  _buildTextContentWithPageIndicator(),
+                  // Selection indicator
+                  _buildPageIndicator(context, _highlightedArtifactIds.length),
                   // Big ol' button
                   Gap(context.insets.xl),
-                  AppTextIconBtn(
-                    'BROWSE ALL ARTIFACTS',
-                    Icons.search,
-                    expand: true,
-                    padding: EdgeInsets.symmetric(vertical: context.insets.md),
-                    onPressed: _handleSearchButtonTap,
-                  ),
+                  _buildBrowseBtn(context),
                   Gap(context.insets.lg),
                 ],
               ),
@@ -256,19 +204,81 @@ class _ArtifactScreenState extends State<ArtifactCarouselScreen> {
     );
   }
 
-  SmoothPageIndicator _buildPageIndicator(BuildContext context, int count) {
-    return SmoothPageIndicator(
-      controller: _controller,
-      count: count,
-      onDotClicked: _changeArtifactIndex,
-      effect: ExpandingDotsEffect(
-          dotWidth: 4,
-          dotHeight: 4,
-          paintStyle: PaintingStyle.fill,
-          strokeWidth: 2,
-          dotColor: context.colors.accent1,
-          activeDotColor: context.colors.accent1,
-          expansionFactor: 4),
+  AppTextIconBtn _buildBrowseBtn(BuildContext context) {
+    return AppTextIconBtn(
+      'BROWSE ALL ARTIFACTS',
+      Icons.search,
+      expand: true,
+      padding: EdgeInsets.symmetric(vertical: context.insets.md),
+      onPressed: _handleSearchButtonTap,
+    );
+  }
+
+  Widget _buildTextContentWithPageIndicator() {
+    return IgnorePointer(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // Wonder Name
+          Text(
+            (_loadedArtifacts.isEmpty ? 'Just a moment...' : _currentArtifact?.culture ?? '').toUpperCase(),
+            style: context.textStyles.titleFont.copyWith(
+              color: context.colors.accent1,
+              fontSize: 14,
+              height: 1.2,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          Gap(context.insets.md),
+
+          _loadedArtifacts.isEmpty
+              ? AppLoader()
+              : FXAnimate(
+                  fx: const [FadeFX()],
+                  key: ValueKey(_currentArtifact?.artifactId),
+                  child: Column(
+                    children: [
+                      // Artifact Title
+                      Text(
+                        _currentArtifact?.title ?? '',
+                        style: context.textStyles.h2.copyWith(color: context.colors.greyStrong),
+                        textAlign: TextAlign.center,
+                        maxLines: 1,
+                      ),
+                      Gap(context.insets.xs),
+
+                      // Time frame
+                      Text(
+                        _currentArtifact?.date ?? '',
+                        style: context.textStyles.body.copyWith(color: context.colors.body),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ),
+
+          Gap(context.insets.lg),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPageIndicator(BuildContext context, int count) {
+    return IgnorePointer(
+      child: SmoothPageIndicator(
+        controller: _controller,
+        count: count,
+        onDotClicked: _changeArtifactIndex,
+        effect: ExpandingDotsEffect(
+            dotWidth: 4,
+            dotHeight: 4,
+            paintStyle: PaintingStyle.fill,
+            strokeWidth: 2,
+            dotColor: context.colors.accent1,
+            activeDotColor: context.colors.accent1,
+            expansionFactor: 4),
+      ),
     );
   }
 }
