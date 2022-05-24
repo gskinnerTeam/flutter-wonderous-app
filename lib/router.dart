9f48330451311da1a575b823fb1d57a7e7429fc6
Shawn <shawn@gskinner.com>
2022-05-23 21:47:42 -0600

    Remove settings view

--- lib/router.dart
+++ lib/router.dart
@@ -8,14 +8,16 @@ import 'package:wonders/ui/screens/artifact/artifact_details/artifact_details_sc
 import 'package:wonders/ui/screens/artifact/artifact_search/artifact_search_screen.dart';
 import 'package:wonders/ui/screens/collection/collection_screen.dart';
 import 'package:wonders/ui/screens/home/wonders_home_screen.dart';
-import 'package:wonders/ui/screens/settings/settings_screen.dart';
 import 'package:wonders/ui/screens/splash/splash_screen.dart';
 import 'package:wonders/ui/screens/timeline/timeline_screen.dart';
 import 'package:wonders/ui/screens/wallpaper_photo/wallpaper_photo_screen.dart';
 import 'package:wonders/ui/screens/wonder_details/wonders_details_screen.dart';
 
+import 'ui/screens/intro/intro_screen.dart';
+
 class ScreenPaths {
   static String splash = '/';
+  static String intro = '/welcome';
   static String home = '/home';
   static String settings = '/settings';
   static String wonderDetails(WonderType type) => '/wonder/${type.name}';
@@ -47,8 +49,8 @@ final appRouter = GoRouter(
   navigatorBuilder: (_, __, child) => WondersAppScaffold(child: child),
   routes: [
     AppRoute(ScreenPaths.splash, (_) => SplashScreen()),
-    AppRoute(ScreenPaths.home, (_) => WondersHomeScreen()),
-    AppRoute(ScreenPaths.settings, (_) => SettingsScreen()),
+    AppRoute(ScreenPaths.home, (_) => HomeScreen()),
+    AppRoute(ScreenPaths.intro, (_) => IntroScreen()),
     AppRoute('/wonder/:type', (s) {
       return WonderDetailsScreen(type: _parseWonderType(s.params['type']!));
     }, useFade: true),
