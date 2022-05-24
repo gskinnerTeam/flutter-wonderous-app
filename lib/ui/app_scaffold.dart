dc20f05c47e955642dc7d16233a9fe33eb1a1d97
Shawn <shawn@gskinner.com>
2022-05-23 21:50:34 -0600

    Remove performance overlay

--- lib/ui/app_scaffold.dart
+++ lib/ui/app_scaffold.dart
@@ -1,4 +1,3 @@
-import 'package:performance/performance.dart';
 import 'package:wonders/common_libs.dart';
 import 'package:wonders/ui/common/app_scroll_behavior.dart';
 
@@ -11,30 +10,25 @@ class WondersAppScaffold extends StatelessWidget with GetItMixin {
     // Construct an AppStyle using the current screen size.
     final styles = AppStyle(screenSize: context.sizePx);
     Animate.defaultDuration = styles.times.fast;
-    // Respect fps meter setting
-    bool enableFpsMeter = watchX((SettingsLogic c) => c.enableFpsMeter);
     // Pass our custom style down to the tree with provider and inject a themData to style existing Material components.
-    return CustomPerformanceOverlay(
-      enabled: enableFpsMeter,
-      child: Stack(
-        children: [
-          Provider<AppStyle>.value(
-            value: styles,
-            child: Theme(
-              data: styles.colors.toThemeData(),
-              // Provide a default texts style to allow Hero's to render text properly
-              child: DefaultTextStyle(
-                style: styles.text.body,
-                //Custom scroll behavior to make responsive testing easier on desktop
-                child: ScrollConfiguration(
-                  behavior: AppScrollBehavior(),
-                  child: child,
-                ),
+    return Stack(
+      children: [
+        Provider<AppStyle>.value(
+          value: styles,
+          child: Theme(
+            data: styles.colors.toThemeData(),
+            // Provide a default texts style to allow Hero's to render text properly
+            child: DefaultTextStyle(
+              style: styles.text.body,
+              //Custom scroll behavior to make responsive testing easier on desktop
+              child: ScrollConfiguration(
+                behavior: AppScrollBehavior(),
+                child: child,
               ),
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
