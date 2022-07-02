5bdf7cb5f330a7430a961d748c0d3969cfa1c892
Shawn <shawn@gskinner.com>
2022-07-02 10:43:53 -0600

    Improve semantics on editorial page

--- lib/ui/screens/editorial/widgets/_title_text.dart
+++ lib/ui/screens/editorial/widgets/_title_text.dart
@@ -6,64 +6,68 @@ class _TitleText extends StatelessWidget {
   final ScrollController scroller;
 
   @override
-  Widget build(BuildContext context) => DefaultTextColor(
-        color: $styles.colors.offWhite,
-        child: Column(
-          children: [
-            Gap($styles.insets.md),
-            Gap(30),
-            SeparatedRow(
-              padding: EdgeInsets.symmetric(horizontal: $styles.insets.sm),
-              separatorBuilder: () => Gap($styles.insets.sm),
-              children: [
-                Expanded(
-                  child: Divider(
-                    color: data.type.fgColor,
-                  ).animate().scale(curve: Curves.easeOut, delay: 500.ms),
-                ),
-                Semantics(
-                  header: true,
-                  sortKey: OrdinalSortKey(1),
-                  child: Text(
-                    data.subTitle.toUpperCase(),
-                    style: $styles.text.title2,
-                  ).animate().fade(delay: 100.ms),
-                ),
-                Expanded(
-                  child: Divider(
-                    color: data.type.fgColor,
-                  ).animate().scale(curve: Curves.easeOut, delay: 500.ms),
-                ),
-              ],
-            ),
-            Gap($styles.insets.md),
-            Semantics(sortKey: OrdinalSortKey(0), child: WonderTitleText(data)),
-            Gap($styles.insets.xs),
-            Text(
-              data.regionTitle.toUpperCase(),
-              style: $styles.text.title1,
-              textAlign: TextAlign.center,
-            ),
-            Gap($styles.insets.md),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: $styles.insets.md),
-              child: AnimatedBuilder(
-                animation: scroller,
-                builder: (_, __) => CompassDivider(
-                  isExpanded: scroller.position.pixels <= 0,
-                  linesColor: data.type.fgColor,
-                  compassColor: $styles.colors.offWhite,
+  Widget build(BuildContext context) => MergeSemantics(
+        child: DefaultTextColor(
+          color: $styles.colors.offWhite,
+          child: Column(
+            children: [
+              Gap($styles.insets.md),
+              Gap(30),
+              SeparatedRow(
+                padding: EdgeInsets.symmetric(horizontal: $styles.insets.sm),
+                separatorBuilder: () => Gap($styles.insets.sm),
+                children: [
+                  Expanded(
+                    child: Divider(
+                      color: data.type.fgColor,
+                    ).animate().scale(curve: Curves.easeOut, delay: 500.ms),
+                  ),
+                  Semantics(
+                    header: true,
+                    sortKey: OrdinalSortKey(1),
+                    child: Text(
+                      data.subTitle.toUpperCase(),
+                      style: $styles.text.title2,
+                    ).animate().fade(delay: 100.ms),
+                  ),
+                  Expanded(
+                    child: Divider(
+                      color: data.type.fgColor,
+                    ).animate().scale(curve: Curves.easeOut, delay: 500.ms),
+                  ),
+                ],
+              ),
+              Gap($styles.insets.md),
+              Semantics(sortKey: OrdinalSortKey(0), child: WonderTitleText(data)),
+              Gap($styles.insets.xs),
+              Text(
+                data.regionTitle.toUpperCase(),
+                style: $styles.text.title1,
+                textAlign: TextAlign.center,
+              ),
+              Gap($styles.insets.md),
+              ExcludeSemantics(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: $styles.insets.md),
+                  child: AnimatedBuilder(
+                    animation: scroller,
+                    builder: (_, __) => CompassDivider(
+                      isExpanded: scroller.position.pixels <= 0,
+                      linesColor: data.type.fgColor,
+                      compassColor: $styles.colors.offWhite,
+                    ),
+                  ),
                 ),
               ),
-            ),
-            Gap($styles.insets.sm),
-            Text(
-              '${StringUtils.formatYr(data.startYr)} - ${StringUtils.formatYr(data.endYr)}',
-              style: $styles.text.h4,
-              textAlign: TextAlign.center,
-            ),
-            Gap($styles.insets.sm),
-          ],
+              Gap($styles.insets.sm),
+              Text(
+                '${StringUtils.formatYr(data.startYr)} to ${StringUtils.formatYr(data.endYr)}',
+                style: $styles.text.h4,
+                textAlign: TextAlign.center,
+              ),
+              Gap($styles.insets.sm),
+            ],
+          ),
         ),
       );
 }
