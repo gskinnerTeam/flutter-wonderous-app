a4e9a5388a621930e83f556d6ccc46638337b7d2
Grant Skinner <grant@gskinner.com>
2022-07-16 16:44:45 -0600

    Add search message for non-english users
    Still needs a check for English

--- lib/logic/settings_logic.dart
+++ lib/logic/settings_logic.dart
@@ -5,17 +5,20 @@ class SettingsLogic with ThrottledSaveLoadMixin {
   @override
   String get fileName => 'settings.dat';
 
-  late final hasCompletedOnboarding = ValueNotifier<bool>(true)..addListener(scheduleSave);
+  late final hasCompletedOnboarding = ValueNotifier<bool>(false)..addListener(scheduleSave);
+  late final hasDismissedSearchMessage = ValueNotifier<bool>(false)..addListener(scheduleSave);
 
   @override
   void copyFromJson(Map<String, dynamic> value) {
     hasCompletedOnboarding.value = value['hasCompletedOnboarding'] ?? false;
+    hasDismissedSearchMessage.value = value['hasDismissedSearchMessage'] ?? false;
   }
 
   @override
   Map<String, dynamic> toJson() {
     return {
       'hasCompletedOnboarding': hasCompletedOnboarding.value,
+      'hasDismissedSearchMessage': hasDismissedSearchMessage.value,
     };
   }
 }
--- lib/ui/screens/artifact/artifact_search/widgets/_results_grid.dart
+++ lib/ui/screens/artifact/artifact_search/widgets/_results_grid.dart
@@ -10,19 +10,57 @@ class _ResultsGrid extends StatelessWidget {
   Widget build(BuildContext context) {
     return RepaintBoundary(
       child: ScrollDecorator.shadow(
-        builder: (controller) => MasonryGridView.count(
-          shrinkWrap: true,
-          addAutomaticKeepAlives: false,
-          padding: EdgeInsets.all($styles.insets.sm).copyWith(bottom: $styles.insets.offset),
+        builder: (controller) => CustomScrollView(
           controller: controller,
-          crossAxisCount: 2,
-          mainAxisSpacing: $styles.insets.sm,
-          crossAxisSpacing: $styles.insets.sm,
-          itemCount: searchResults.length,
           clipBehavior: Clip.hardEdge,
-          itemBuilder: (context, index) => _ResultTile(onPressed: onPressed, data: searchResults[index]),
+          shrinkWrap: true,
+          slivers: [
+            SliverToBoxAdapter(child: _buildLanguageMessage()),
+            SliverPadding(
+              padding: EdgeInsets.all($styles.insets.sm).copyWith(bottom: $styles.insets.offset),
+              sliver: SliverMasonryGrid.count(
+                crossAxisCount: 2,
+                mainAxisSpacing: $styles.insets.sm,
+                crossAxisSpacing: $styles.insets.sm,
+                childCount: searchResults.length,
+                itemBuilder: (context, index) => _ResultTile(onPressed: onPressed, data: searchResults[index]),
+              ),
+            ),
+          ],
         ),
       ),
+    );
+  }
+
+  Widget _buildLanguageMessage() {
+    // TODO: wire up the check for English.
+    bool isEnglish = false;
+    return ValueListenableBuilder<bool>(
+      valueListenable: settingsLogic.hasDismissedSearchMessage,
+      builder: (_, value, __) {
+        //if (isEnglish || value) return SizedBox();
+        return AppBtn.basic(
+          onPressed: () => settingsLogic.hasDismissedSearchMessage.value = true,
+          semanticLabel: 'dismiss message',
+          child: Container(
+            color: $styles.colors.offWhite.withOpacity(0.1),
+            padding: EdgeInsets.all($styles.insets.sm),
+            child: Row(
+              children: [
+                Flexible(
+                  child: Text(
+                    'This content is provided by the Metropolitan Museum of Art Collection API, and is only available in English.',
+                  ),
+                ),
+                Icon(
+                  Icons.close,
+                  size: $styles.insets.md,
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
