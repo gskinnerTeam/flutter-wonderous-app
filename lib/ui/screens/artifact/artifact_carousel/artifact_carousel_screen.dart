b729352f1d38fe6cc2a973855d42a622b8148dd9
Shawn <shawn@gskinner.com>
2022-07-28 12:12:32 -0600

    Polish accessibility on carousel screen

--- lib/ui/screens/artifact/artifact_carousel/artifact_carousel_screen.dart
+++ lib/ui/screens/artifact/artifact_carousel/artifact_carousel_screen.dart
@@ -27,6 +27,13 @@ class _ArtifactScreenState extends State<ArtifactCarouselScreen> {
   late List<HighlightData> _artifacts;
   late PageController _controller;
 
+  double get _currentOffset {
+    bool inited = _controller.hasClients && _controller.position.haveDimensions;
+    return inited ? _controller.page! : _controller.initialPage * 1.0;
+  }
+
+  int get _currentIndex => _currentOffset.round() % _artifacts.length;
+
   @override
   void initState() {
     super.initState();
@@ -45,9 +52,9 @@ class _ArtifactScreenState extends State<ArtifactCarouselScreen> {
     super.dispose();
   }
 
-  void _handlePageBtn(int delta) {
+  void _handlePageTap(int delta) {
     _controller.animateToPage(
-      currentOffset.round() + delta,
+      _currentOffset.round() + delta,
       duration: $styles.times.fast,
       curve: Curves.easeOut,
     );
@@ -58,30 +65,23 @@ class _ArtifactScreenState extends State<ArtifactCarouselScreen> {
     context.push(ScreenPaths.artifact(data.artifactId));
   }
 
-  void _handleSearchButtonTap() {
+  void _handleSearchTap() {
     context.push(ScreenPaths.search(widget.type));
   }
-
-  double get currentOffset {
-    bool inited = _controller.hasClients && _controller.position.haveDimensions;
-    return inited ? _controller.page! : _controller.initialPage * 1.0;
-  }
-
-  int get currentIndex => currentOffset.round() % _artifacts.length;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       color: $styles.colors.greyStrong,
-      child: AnimatedBuilder(animation: _controller, builder: _screenBuilder),
+      child: AnimatedBuilder(animation: _controller, builder: _buildScreen),
     );
   }
 
-  Widget _screenBuilder(BuildContext context, _) {
+  Widget _buildScreen(BuildContext context, _) {
     final double backdropWidth = math.min(context.widthPx, _maxElementWidth);
     final double backdropHeight = math.min(context.heightPx * 0.65, _maxElementHeight);
     final bool small = backdropHeight / _maxElementHeight < 0.7;
-    final HighlightData artifact = _artifacts[currentIndex];
+    final HighlightData artifact = _artifacts[_currentIndex];
 
     return Stack(children: [
       Positioned.fill(child: ArtifactCarouselBg(url: artifact.imageUrl)),
@@ -106,16 +106,21 @@ class _ArtifactScreenState extends State<ArtifactCarouselScreen> {
               controller: _controller,
               clipBehavior: Clip.none,
               itemBuilder: (context, index) {
+                bool isCurrentIndex = index % _artifacts.length == _currentIndex;
                 return ExcludeSemantics(
-                  excluding: index != currentIndex,
-                  child: ArtifactCarouselImage(
-                    index: index,
-                    currentPage: currentOffset,
-                    artifact: _artifacts[index % _artifacts.length],
-                    bottomPadding: backdropHeight,
-                    maxWidth: backdropWidth,
-                    maxHeight: backdropHeight,
-                    onPressed: () => _handleArtifactTap(index),
+                  excluding: isCurrentIndex == false,
+                  child: Semantics(
+                    // Reads content as it changes without the user focus on it.
+                    liveRegion: true,
+                    child: ArtifactCarouselImage(
+                      index: index,
+                      currentPage: _currentOffset,
+                      artifact: _artifacts[index % _artifacts.length],
+                      bottomPadding: backdropHeight,
+                      maxWidth: backdropWidth,
+                      maxHeight: backdropHeight,
+                      onPressed: () => _handleArtifactTap(index),
+                    ),
                   ),
                 );
               },
@@ -140,7 +145,7 @@ class _ArtifactScreenState extends State<ArtifactCarouselScreen> {
                       text: 'Browse all artifacts',
                       icon: Icons.search,
                       expand: true,
-                      onPressed: _handleSearchButtonTap,
+                      onPressed: _handleSearchTap,
                     ),
                     Gap(small ? $styles.insets.md : $styles.insets.lg),
                   ],
@@ -162,7 +167,7 @@ class _ArtifactScreenState extends State<ArtifactCarouselScreen> {
         excludeFromSemantics: true,
         onTapUp: (_) {
           HapticFeedback.mediumImpact();
-          _handlePageBtn(offset);
+          _handlePageTap(offset);
         },
         behavior: HitTestBehavior.translucent,
       ),
@@ -170,15 +175,15 @@ class _ArtifactScreenState extends State<ArtifactCarouselScreen> {
   }
 
   Widget _buildContent(BuildContext context, HighlightData artifact, double width, bool small) {
-    return IgnorePointer(
-      child: Container(
-        width: width,
-        padding: EdgeInsets.symmetric(horizontal: $styles.insets.sm),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
+    return Container(
+      width: width,
+      padding: EdgeInsets.symmetric(horizontal: $styles.insets.sm),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ExcludeSemantics(
+            child: Padding(
               padding: EdgeInsets.symmetric(horizontal: width * 0.1),
               child: Text(
                 artifact.culture.toUpperCase(),
@@ -192,8 +197,10 @@ class _ArtifactScreenState extends State<ArtifactCarouselScreen> {
                 overflow: TextOverflow.ellipsis,
               ),
             ),
-            Gap($styles.insets.xxs),
-            Column(
+          ),
+          Gap($styles.insets.xxs),
+          ExcludeSemantics(
+            child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
                 Container(
@@ -211,10 +218,10 @@ class _ArtifactScreenState extends State<ArtifactCarouselScreen> {
                 Text(artifact.date, style: $styles.text.body, textAlign: TextAlign.center),
               ],
             ).animate(key: ValueKey(artifact.artifactId)).fadeIn(),
-            Gap(small ? $styles.insets.sm : $styles.insets.lg),
-            AppPageIndicator(count: _artifacts.length, controller: _controller, semanticPageTitle: 'artifact'),
-          ],
-        ),
+          ),
+          Gap(small ? $styles.insets.sm : $styles.insets.lg),
+          AppPageIndicator(count: _artifacts.length, controller: _controller, semanticPageTitle: 'artifact'),
+        ],
       ),
     );
   }
