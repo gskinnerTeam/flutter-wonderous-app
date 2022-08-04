e83d1596093acc7c6bfe118d5da5d80fd93235f4
Grant Skinner <grant@gskinner.com>
2022-08-04 14:51:36 -0600

    Fix layout issues

--- lib/ui/common/modals/fullscreen_url_img_viewer.dart
+++ lib/ui/common/modals/fullscreen_url_img_viewer.dart
@@ -2,7 +2,7 @@ import 'package:image_fade/image_fade.dart';
 import 'package:wonders/common_libs.dart';
 import 'package:wonders/ui/common/controls/app_loader.dart';
 
-//TODO: Add required semantic to all fullscreen images?
+//TODO: Add proper semantic label.
 class FullscreenUrlImgViewer extends StatelessWidget {
   const FullscreenUrlImgViewer({Key? key, required this.url}) : super(key: key);
   final String url;
@@ -13,17 +13,18 @@ class FullscreenUrlImgViewer extends StatelessWidget {
       color: $styles.colors.greyStrong,
       child: Stack(
         children: [
-          Semantics(
-            label: 'Full screen',
-            container: true,
-            image: true,
-            child: ExcludeSemantics(
-              child: Positioned.fill(
+          Positioned.fill(
+            child: Semantics(
+              label: 'Full screen',
+              container: true,
+              image: true,
+              child: ExcludeSemantics(
                 child: InteractiveViewer(
                   child: Hero(
                     tag: url,
                     child: ImageFade(
                       image: NetworkImage(url),
+                      fit: BoxFit.contain,
                       loadingBuilder: (_, __, ___) => const Center(child: AppLoader()),
                     ),
                   ),
