384847d68b0c3de31322f21a183071f316f73e7b
Grant Skinner <grant@gskinner.com>
2022-08-15 13:44:31 -0600

    Use ImageFiltered for better performance

--- lib/ui/screens/artifact/artifact_carousel/widgets/_blurred_image_bg.dart
+++ lib/ui/screens/artifact/artifact_carousel/widgets/_blurred_image_bg.dart
@@ -10,21 +10,17 @@ class _BlurredImageBg extends StatelessWidget {
     return Transform.scale(
       scale: 1.25,
       alignment: Alignment(0, 0.8),
-      child: Stack(children: [
-        Positioned.fill(
+      child: Container(
+        foregroundDecoration: BoxDecoration(color: $styles.colors.black.withOpacity(0.6)),
+        child: ImageFiltered(
+          imageFilter: ImageFilter.blur(sigmaX: 6, sigmaY: 6),
           child: AppImage(
             image: url == null ? null : NetworkImage(url!),
             fit: BoxFit.cover,
             scale: 0.5,
           ),
         ),
-        Positioned.fill(
-          child: BackdropFilter(
-            filter: ImageFilter.blur(sigmaX: 6, sigmaY: 6),
-            child: Container(color: $styles.colors.black.withOpacity(0.6)),
-          ),
-        ),
-      ]),
+      ),
     );
   }
 }
