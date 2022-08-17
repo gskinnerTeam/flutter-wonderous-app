b6459751acb95bdc8fd4cc10f51a01d0a3d488be
Grant Skinner <grant@gskinner.com>
2022-08-17 10:49:50 -0600

    Error handling for Met API

--- lib/logic/met_api_logic.dart
+++ lib/logic/met_api_logic.dart
@@ -1,8 +1,11 @@
 import 'dart:collection';
 
 import 'package:wonders/common_libs.dart';
+import 'package:wonders/logic/common/string_utils.dart';
 import 'package:wonders/logic/data/artifact_data.dart';
 import 'package:wonders/logic/met_api_service.dart';
+
+import 'package:wonders/logic/common/http_client.dart';
 
 class MetAPILogic {
   final HashMap<String, ArtifactData?> _artifactCache = HashMap();
@@ -12,7 +15,9 @@ class MetAPILogic {
   /// Returns artifact data by ID. Returns null if artifact cannot be found. */
   Future<ArtifactData?> getArtifactByID(String id) async {
     if (_artifactCache.containsKey(id)) return _artifactCache[id];
-    ArtifactData? results = (await service.getObjectByID(id)).content;
-    return _artifactCache[id] = results;
+    ServiceResult<ArtifactData?> result = (await service.getObjectByID(id));
+    if (!result.success) throw StringUtils.supplant($strings.artifactDetailsErrorNotFound, {'{artifactId}': id});
+    ArtifactData? artifact = result.content;
+    return _artifactCache[id] = artifact;
   }
 }
