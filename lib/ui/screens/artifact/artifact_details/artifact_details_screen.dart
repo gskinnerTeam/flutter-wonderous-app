bfd14267d6de02a108446b49c2b71cf50066a854
Grant Skinner <grant@gskinner.com>
2022-08-17 10:50:42 -0600

    UI for artifact detail load errors.

--- lib/ui/screens/artifact/artifact_details/artifact_details_screen.dart
+++ lib/ui/screens/artifact/artifact_details/artifact_details_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:wonders/common_libs.dart';
 import 'package:wonders/logic/common/string_utils.dart';
 import 'package:wonders/logic/data/artifact_data.dart';
-import 'package:wonders/ui/common/app_loading_error.dart';
 import 'package:wonders/ui/common/compass_divider.dart';
 import 'package:wonders/ui/common/controls/app_loading_indicator.dart';
 import 'package:wonders/ui/common/gradient_container.dart';
@@ -30,27 +29,41 @@ class _ArtifactDetailsScreenState extends State<ArtifactDetailsScreen> {
         future: _future,
         builder: (_, snapshot) {
           final data = snapshot.data;
-          if (snapshot.hasData && data == null) {
-            return AppLoadError(label: StringUtils.supplant($strings.artifactDetailsErrorNotFound, {'{artifactId}': widget.artifactId}));
+          late Widget content;
+          if (snapshot.hasError || (snapshot.hasData && data == null)) {
+            content = Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(child: Icon(Icons.warning_amber_outlined, color: $styles.colors.accent1, size: $styles.insets.lg,)),
+                Gap($styles.insets.xs),
+                SizedBox(width: $styles.insets.xxl * 3, child: Text(
+                  StringUtils.supplant($strings.artifactDetailsErrorNotFound, {'{artifactId}': widget.artifactId}),
+                  style: $styles.text.body.copyWith(color: $styles.colors.offWhite),
+                  textAlign: TextAlign.center,
+                ),),
+              ],
+            ).animate().fadeIn();
+          } else if (!snapshot.hasData) {
+            content = Center(child: AppLoadingIndicator());
+          } else {
+            content = CustomScrollView(
+              slivers: [
+                SliverAppBar(
+                  pinned: true,
+                  elevation: 0,
+                  leading: SizedBox.shrink(),
+                  expandedHeight: context.heightPx * .5,
+                  collapsedHeight: context.heightPx * .35,
+                  flexibleSpace: _Header(data: data!),
+                ),
+                SliverToBoxAdapter(child: _Content(data: data)),
+              ],
+            );
           }
 
           return Stack(children: [
-            /// Content
-            !snapshot.hasData
-                ? Center(child: AppLoadingIndicator())
-                : CustomScrollView(
-                    slivers: [
-                      SliverAppBar(
-                        pinned: true,
-                        elevation: 0,
-                        leading: SizedBox.shrink(),
-                        expandedHeight: context.heightPx * .5,
-                        collapsedHeight: context.heightPx * .35,
-                        flexibleSpace: _Header(data: data!),
-                      ),
-                      SliverToBoxAdapter(child: _Content(data: data)),
-                    ],
-                  ),
+            content,
             BackBtn().safe(),
           ]);
         },
