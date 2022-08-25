1a9520353456c6b870163b44fe969c823e2219d7
Shawn <shawn@gskinner.com>
2022-08-25 08:45:52 -0600

    Imrpove semantics on timeline

--- lib/ui/screens/timeline/widgets/_year_markers.dart
+++ lib/ui/screens/timeline/widgets/_year_markers.dart
@@ -54,11 +54,13 @@ class _YearMarker extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment(0, -1 + offset * 2),
-      child: FractionalTranslation(
-        translation: Offset(0, 0),
-        child: Text('${yr.abs()}', style: $styles.text.body.copyWith(color: Colors.white, height: 1)),
+    return ExcludeSemantics(
+      child: Align(
+        alignment: Alignment(0, -1 + offset * 2),
+        child: FractionalTranslation(
+          translation: Offset(0, 0),
+          child: Text('${yr.abs()}', style: $styles.text.body.copyWith(color: Colors.white, height: 1)),
+        ),
       ),
     );
   }
