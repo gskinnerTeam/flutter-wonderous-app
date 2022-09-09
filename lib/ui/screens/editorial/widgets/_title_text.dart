e0fc2e75ac15abd72ca1504b0128c12245317145
Shawn <shawn@shawnblais.com>
2022-09-09 11:53:32 -0600

    Disable hero animation when Title is scrolled out of view

--- lib/ui/screens/editorial/widgets/_title_text.dart
+++ lib/ui/screens/editorial/widgets/_title_text.dart
@@ -6,89 +6,97 @@ class _TitleText extends StatelessWidget {
   final ScrollController scroller;
 
   @override
-  Widget build(BuildContext context) => MergeSemantics(
-        child: DefaultTextColor(
-          color: $styles.colors.offWhite,
-          child: StaticTextScale(
-            child: Column(
-              children: [
-                Gap($styles.insets.md),
-                Gap(30),
+  Widget build(BuildContext context) {
+    return MergeSemantics(
+      child: DefaultTextColor(
+        color: $styles.colors.offWhite,
+        child: StaticTextScale(
+          child: Column(
+            children: [
+              Gap($styles.insets.md),
+              Gap(30),
 
-                /// Sub-title row
-                SeparatedRow(
-                  padding: EdgeInsets.symmetric(horizontal: $styles.insets.sm),
-                  separatorBuilder: () => Gap($styles.insets.sm),
-                  children: [
-                    Expanded(
-                      child: Divider(
-                        color: data.type.fgColor,
-                      ).animate().scale(curve: Curves.easeOut, delay: 500.ms),
-                    ),
-                    Semantics(
-                      header: true,
-                      sortKey: OrdinalSortKey(1),
-                      child: Text(
-                        data.subTitle.toUpperCase(),
-                        style: $styles.text.title2,
-                      ).animate().fade(delay: 100.ms),
-                    ),
-                    Expanded(
-                      child: Divider(
-                        color: data.type.fgColor,
-                      ).animate().scale(curve: Curves.easeOut, delay: 500.ms),
-                    ),
-                  ],
-                ),
-                Gap($styles.insets.md),
+              /// Sub-title row
+              SeparatedRow(
+                padding: EdgeInsets.symmetric(horizontal: $styles.insets.sm),
+                separatorBuilder: () => Gap($styles.insets.sm),
+                children: [
+                  Expanded(
+                    child: Divider(
+                      color: data.type.fgColor,
+                    ).animate().scale(curve: Curves.easeOut, delay: 500.ms),
+                  ),
+                  Semantics(
+                    header: true,
+                    sortKey: OrdinalSortKey(1),
+                    child: Text(
+                      data.subTitle.toUpperCase(),
+                      style: $styles.text.title2,
+                    ).animate().fade(delay: 100.ms),
+                  ),
+                  Expanded(
+                    child: Divider(
+                      color: data.type.fgColor,
+                    ).animate().scale(curve: Curves.easeOut, delay: 500.ms),
+                  ),
+                ],
+              ),
+              Gap($styles.insets.md),
 
-                /// Wonder title text
-                Semantics(
-                  sortKey: OrdinalSortKey(0),
-                  child: WonderTitleText(data),
-                ),
-                Gap($styles.insets.xs),
+              /// Wonder title text
+              Semantics(
+                sortKey: OrdinalSortKey(0),
+                child: AnimatedBuilder(
+                    animation: scroller,
+                    builder: (_, __) {
+                      final yPos = ContextUtils.getGlobalPos(context)?.dy ?? 0;
+                      bool enableHero = yPos > -100;
+                      return WonderTitleText(data, enableHero: enableHero);
+                    }),
+              ),
+              Gap($styles.insets.xs),
 
-                /// Region
-                Text(
-                  data.regionTitle.toUpperCase(),
-                  style: $styles.text.title1,
-                  textAlign: TextAlign.center,
-                ),
-                Gap($styles.insets.md),
+              /// Region
+              Text(
+                data.regionTitle.toUpperCase(),
+                style: $styles.text.title1,
+                textAlign: TextAlign.center,
+              ),
+              Gap($styles.insets.md),
 
-                /// Compass divider
-                ExcludeSemantics(
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: $styles.insets.md),
-                    child: AnimatedBuilder(
-                      animation: scroller,
-                      builder: (_, __) => CompassDivider(
-                        isExpanded: scroller.position.pixels <= 0,
-                        linesColor: data.type.fgColor,
-                        compassColor: $styles.colors.offWhite,
-                      ),
+              /// Compass divider
+              ExcludeSemantics(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: $styles.insets.md),
+                  child: AnimatedBuilder(
+                    animation: scroller,
+                    builder: (_, __) => CompassDivider(
+                      isExpanded: scroller.position.pixels <= 0,
+                      linesColor: data.type.fgColor,
+                      compassColor: $styles.colors.offWhite,
                     ),
                   ),
                 ),
-                Gap($styles.insets.sm),
+              ),
+              Gap($styles.insets.sm),
 
-                /// Date
-                Text(
-                  StringUtils.supplant(
-                    $strings.titleLabelDate,
-                    {
-                      '{fromDate}': StringUtils.formatYr(data.startYr),
-                      '{endDate}': StringUtils.formatYr(data.endYr),
-                    },
-                  ),
-                  style: $styles.text.h4,
-                  textAlign: TextAlign.center,
+              /// Date
+              Text(
+                StringUtils.supplant(
+                  $strings.titleLabelDate,
+                  {
+                    '{fromDate}': StringUtils.formatYr(data.startYr),
+                    '{endDate}': StringUtils.formatYr(data.endYr),
+                  },
                 ),
-                Gap($styles.insets.sm),
-              ],
-            ),
+                style: $styles.text.h4,
+                textAlign: TextAlign.center,
+              ),
+              Gap($styles.insets.sm),
+            ],
           ),
         ),
-      );
+      ),
+    );
+  }
 }
