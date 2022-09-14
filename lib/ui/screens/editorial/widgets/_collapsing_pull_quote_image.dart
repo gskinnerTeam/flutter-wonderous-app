521e6d76ba2fa1d74c5960d66e131fe4ee2f2a2e
Kenzie Schmoll <kenzieschmoll@google.com>
2022-09-13 17:45:15 -0700

    fix opacity

--- lib/ui/screens/editorial/widgets/_collapsing_pull_quote_image.dart
+++ lib/ui/screens/editorial/widgets/_collapsing_pull_quote_image.dart
@@ -130,8 +130,8 @@ class _CollapsingPullQuoteImage extends StatelessWidget {
         ),
         GradientContainer(
           [
-            Color(0xFFBEABA1).withOpacity(.9),
-            Color(0xFFA6958C).withOpacity(.9),
+            Color(0xFFBEABA1).withOpacity(1),
+            Color(0xFFA6958C).withOpacity(1),
           ],
           const [0.0, 1.0],
           blendMode: BlendMode.colorBurn,
