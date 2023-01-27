acdb6acd5f60eb0e840cbdb0dbc4577fe46733dc
sarbagyastha <sarbagyastha@gmail.com>
2023-01-27 11:12:07 +0545

    :art: swap `flutter_inappwebview` with `webview_flutter` plugin

--- lib/ui/common/modals/fullscreen_web_view.dart
+++ lib/ui/common/modals/fullscreen_web_view.dart
@@ -1,18 +1,29 @@
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 import 'package:wonders/common_libs.dart';
 
-class FullscreenWebView extends StatelessWidget {
-  const FullscreenWebView(this.url, {Key? key}) : super(key: key);
+class FullscreenWebView extends StatefulWidget {
+  const FullscreenWebView(this.url, {super.key});
   final String url;
+
+  @override
+  State<FullscreenWebView> createState() => _FullscreenWebViewState();
+}
+
+class _FullscreenWebViewState extends State<FullscreenWebView> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()..loadRequest(Uri.parse(widget.url));
+  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(),
-        body: InAppWebView(
-          initialUrlRequest: URLRequest(url: Uri.parse(url)),
-        ),
+        body: WebViewWidget(controller: _controller),
       ),
     );
   }
