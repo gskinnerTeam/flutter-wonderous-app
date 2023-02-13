d2b44a4dc63be9d1c214668a29cb339c542f640a
Shawn <shawn@gskinner.com>
2023-02-13 10:39:07 -0700

    cleanup

--- lib/ui/app_scaffold.dart
+++ lib/ui/app_scaffold.dart
@@ -1,8 +1,8 @@
 import 'package:wonders/common_libs.dart';
 import 'package:wonders/ui/common/app_scroll_behavior.dart';
 
-class WondersAppScaffold extends StatelessWidget with GetItMixin {
-  WondersAppScaffold({Key? key, required this.child}) : super(key: key);
+class WondersAppScaffold extends StatelessWidget {
+  const WondersAppScaffold({Key? key, required this.child}) : super(key: key);
   final Widget child;
   static AppStyle get style => _style;
   static AppStyle _style = AppStyle();
