6e5a7152837df8a500cf303afd2de9547badff06
Shawn <shawn@gskinner.com>
2023-02-16 00:32:07 -0700

    Update youtube plugin, and switch to flutter_webview for inline web views

--- lib/ui/common/modals/fullscreen_video_viewer.dart
+++ lib/ui/common/modals/fullscreen_video_viewer.dart
@@ -1,6 +1,6 @@
 import 'package:wonders/common_libs.dart';
 import 'package:wonders/ui/common/controls/app_loading_indicator.dart';
-// import 'package:youtube_player_iframe/youtube_player_iframe.dart';
+import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 
 class FullscreenVideoViewer extends StatefulWidget {
   const FullscreenVideoViewer({Key? key, required this.id}) : super(key: key);
@@ -11,46 +11,44 @@ class FullscreenVideoViewer extends StatefulWidget {
 }
 
 class _FullscreenVideoViewerState extends State<FullscreenVideoViewer> {
+  late final _controller = YoutubePlayerController.fromVideoId(
+    videoId: widget.id,
+    params: const YoutubePlayerParams(showFullscreenButton: true),
+  );
+
   @override
-  Widget build(BuildContext context) => Placeholder();
-  // late final _controller = YoutubePlayerController.fromVideoId(
-  //   videoId: widget.id,
-  //   params: const YoutubePlayerParams(showFullscreenButton: true),
-  // );
-  //
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   appLogic.supportedOrientationsOverride = [Axis.horizontal, Axis.vertical];
-  // }
-  //
-  // @override
-  // void dispose() {
-  //   // when view closes, remove the override
-  //   appLogic.supportedOrientationsOverride = null;
-  //   super.dispose();
-  // }
-  //
-  // @override
-  // Widget build(BuildContext context) {
-  //   double aspect = context.isLandscape ? MediaQuery.of(context).size.aspectRatio : 9 / 9;
-  //   return Scaffold(
-  //     backgroundColor: Colors.black,
-  //     body: YoutubePlayerScaffold(
-  //       controller: _controller,
-  //       aspectRatio: aspect,
-  //       builder: (_, player) => Stack(
-  //         children: [
-  //           player,
-  //           SafeArea(
-  //             child: Padding(
-  //               padding: EdgeInsets.all($styles.insets.md),
-  //               child: const BackBtn(),
-  //             ),
-  //           ),
-  //         ],
-  //       ),
-  //     ),
-  //   );
-  // }
+  void initState() {
+    super.initState();
+    appLogic.supportedOrientationsOverride = [Axis.horizontal, Axis.vertical];
+  }
+
+  @override
+  void dispose() {
+    // when view closes, remove the override
+    appLogic.supportedOrientationsOverride = null;
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double aspect = context.isLandscape ? MediaQuery.of(context).size.aspectRatio : 9 / 9;
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: YoutubePlayerScaffold(
+        controller: _controller,
+        aspectRatio: aspect,
+        builder: (_, player) => Stack(
+          children: [
+            player,
+            SafeArea(
+              child: Padding(
+                padding: EdgeInsets.all($styles.insets.md),
+                child: const BackBtn(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
--- lib/ui/common/modals/fullscreen_web_view.dart
+++ lib/ui/common/modals/fullscreen_web_view.dart
@@ -1,17 +1,22 @@
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 import 'package:wonders/common_libs.dart';
 
 class FullscreenWebView extends StatelessWidget {
-  const FullscreenWebView(this.url, {Key? key}) : super(key: key);
+  FullscreenWebView(this.url, {Key? key}) : super(key: key);
   final String url;
+
+  late final controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..setBackgroundColor(const Color(0x00000000))
+    ..loadRequest(Uri.parse(url));
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(),
-        body: InAppWebView(
-          initialUrlRequest: URLRequest(url: Uri.parse(url)),
+        body: WebViewWidget(
+          controller: controller,
         ),
       ),
     );
