92038ed12a34e27aa2d86f94daf26dbc5891e813
Shawn <shawn@gskinner.com>
2023-02-16 00:51:42 -0700

    Upgrade all deps to latest versions

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,10 +6,10 @@ import FlutterMacOS
 import Foundation
 
 import desktop_window
-import package_info_plus_macos
-import path_provider_macos
+import package_info_plus
+import path_provider_foundation
 import share_plus
-import shared_preferences_macos
+import shared_preferences_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
