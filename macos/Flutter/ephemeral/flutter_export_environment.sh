567eb38bea3dd0644aa257748caa6c0fa217ee30
Shawn <shawn@gskinner.com>
2023-02-16 10:59:36 -0700

    Fix version issues with collections pkg and flutter 3.7.3

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/shawn/Dev/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/shawn/Dev/gskinner/flutter-wonders-app"
+export "FLUTTER_ROOT=C:\_dev\sdks\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\_dev\gskinner\flutter_wonders_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=2.0.3"
-export "FLUTTER_BUILD_NUMBER=2.0.3"
+export "FLUTTER_BUILD_NAME=2.0.10"
+export "FLUTTER_BUILD_NUMBER=2.0.10"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
