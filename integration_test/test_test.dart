c578112d8f6b34d5b099ec1f31a40dde9635a3b1
Julia Borkowska <julia.borkowska@leancode.pl>
2023-03-06 11:07:58 +0100

    Refactor going through intro

--- integration_test/test_test.dart
+++ integration_test/test_test.dart
@@ -1,31 +1,36 @@
 import 'package:patrol/patrol.dart';
 import 'package:wonders/common_libs.dart';
+import 'package:wonders/patrol_keys.dart';
 
 void main() {
   patrolTest(
     'first experimental test',
     nativeAutomation: true,
-    // bindingType: BindingType.integrationTest,
     ($) async {
       prepareApp();
       await $.pumpWidget(WondersApp());
       await initializeApp();
       await $('Swipe left to continue').waitUntilVisible();
 
-      await _swipeLeft($);
-      await _swipeLeft($);
-      await $(#finishIntroButton).tap();
-      await $(#hamburgerMenuButton).tap();
-      await $('About this app').tap();
+      // await _swipeLeft($);
+      // await _swipeLeft($);
+
+      await $(K.finishIntroButton)
+          .which<AnimatedOpacity>((button) => button.opacity == 1)
+          .$(CircleIconBtn)
+          .scrollTo()
+          .tap();
+      await $(K.hamburgerMenuButton).waitUntilVisible();
     },
   );
 }
 
-Future<void> _swipeLeft(PatrolTester $) async {
-  await $.pumpAndSettle(duration: Duration(seconds: 2));
-  await $.native.swipe(
-    from: Offset(0.9, 0.5),
-    to: Offset(0.2, 0.5),
-    steps: 1,
-  );
-}
+// Future<void> _swipeLeft(PatrolTester $) async {
+//   await $.native.swipe(
+//     from: Offset(0.9, 0.5),
+//     to: Offset(0.2, 0.5),
+//     steps: 1,
+//   );
+//   await $.tester.dragFrom(Offset(250, 100), Offset(50, 100));
+//   await $.pumpAndSettle();
+// }
