91835c7ef5436bd782a7f35f942af1ca78bc5c72
Martino Yovo <martinosoyen@gmail.com>
2023-05-27 17:12:03 +0000

    Removed the yellow lines under the suggestion items text

--- lib/ui/screens/artifact/artifact_search/widgets/_search_input.dart
+++ lib/ui/screens/artifact/artifact_search/widgets/_search_input.dart
@@ -9,15 +9,13 @@ class _SearchInput extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
-      builder: (ctx, constraints) => Center(
-        child: Autocomplete<String>(
-          displayStringForOption: (data) => data,
-          onSelected: onSubmit,
-          optionsBuilder: _getSuggestions,
-          optionsViewBuilder: (context, onSelected, results) =>
-              _buildSuggestionsView(context, onSelected, results, constraints),
-          fieldViewBuilder: _buildInput,
-        ),
+      builder: (ctx, constraints) => Autocomplete<String>(
+        displayStringForOption: (data) => data,
+        onSelected: onSubmit,
+        optionsBuilder: _getSuggestions,
+        optionsViewBuilder: (context, onSelected, results) =>
+            _buildSuggestionsView(context, onSelected, results, constraints),
+        fieldViewBuilder: _buildInput,
       ),
     );
   }
@@ -34,49 +32,36 @@ class _SearchInput extends StatelessWidget {
   Widget _buildSuggestionsView(BuildContext context, onSelected, Iterable<String> results, BoxConstraints constraints) {
     List<Widget> items = results.map((str) => _buildSuggestion(context, str, () => onSelected(str))).toList();
     items.insert(0, _buildSuggestionTitle(context));
-    return Stack(
-      children: [
-        ExcludeSemantics(
-          child: AppBtn.basic(
-            onPressed: FocusManager.instance.primaryFocus!.unfocus,
-            semanticLabel: '',
-            child: SizedBox.expand(),
-          ),
+
+    return TopLeft(
+      child: Container(
+        margin: EdgeInsets.only(top: $styles.insets.xxs),
+        width: constraints.maxWidth,
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: $styles.colors.black.withOpacity(0.25),
+              blurRadius: 4,
+              offset: Offset(0, 4),
+            ),
+          ],
         ),
-        TopLeft(
-          child: Material(
-            color: Colors.transparent,
-            child: Container(
-              margin: EdgeInsets.only(top: $styles.insets.xxs),
-              width: constraints.maxWidth,
-              decoration: BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                    color: $styles.colors.black.withOpacity(0.25),
-                    blurRadius: 4,
-                    offset: Offset(0, 4),
-                  ),
-                ],
-              ),
-              child: Container(
-                padding: EdgeInsets.all($styles.insets.xs),
-                decoration: BoxDecoration(
-                  color: $styles.colors.offWhite.withOpacity(0.92),
-                  borderRadius: BorderRadius.circular($styles.insets.xs),
-                ),
-                child: ConstrainedBox(
-                  constraints: BoxConstraints(maxHeight: 200),
-                  child: ListView(
-                    padding: EdgeInsets.all($styles.insets.xs),
-                    shrinkWrap: true,
-                    children: items,
-                  ),
-                ),
-              ),
+        child: Container(
+          padding: EdgeInsets.all($styles.insets.xs),
+          decoration: BoxDecoration(
+            color: $styles.colors.offWhite.withOpacity(0.92),
+            borderRadius: BorderRadius.circular($styles.insets.xs),
+          ),
+          child: ConstrainedBox(
+            constraints: BoxConstraints(maxHeight: 200),
+            child: ListView(
+              padding: EdgeInsets.all($styles.insets.xs),
+              shrinkWrap: true,
+              children: items,
             ),
           ),
         ),
-      ],
+      ),
     );
   }
 
@@ -86,11 +71,13 @@ class _SearchInput extends StatelessWidget {
       margin: EdgeInsets.only(bottom: $styles.insets.xxs),
       decoration: BoxDecoration(border: Border(bottom: BorderSide(color: $styles.colors.greyStrong.withOpacity(0.1)))),
       child: CenterLeft(
-        child: Text(
-          $strings.searchInputTitleSuggestions.toUpperCase(),
-          overflow: TextOverflow.ellipsis,
-          textHeightBehavior: TextHeightBehavior(applyHeightToFirstAscent: false),
+        child: DefaultTextStyle(
           style: $styles.text.title2.copyWith(color: $styles.colors.black),
+          child: Text(
+            $strings.searchInputTitleSuggestions.toUpperCase(),
+            overflow: TextOverflow.ellipsis,
+            textHeightBehavior: TextHeightBehavior(applyHeightToFirstAscent: false),
+          ),
         ),
       ),
     );
@@ -124,53 +111,51 @@ class _SearchInput extends StatelessWidget {
         color: $styles.colors.offWhite,
         borderRadius: BorderRadius.circular($styles.insets.xs),
       ),
-      child: Row(
-        children: [
-          Gap($styles.insets.xs * 1.5),
-          Icon(Icons.search, color: $styles.colors.caption),
-          Expanded(
-            child: TextField(
-              onSubmitted: onSubmit,
-              controller: textController,
-              focusNode: focusNode,
-              style: TextStyle(color: captionColor),
-              textAlignVertical: TextAlignVertical.top,
-              decoration: InputDecoration(
-                isDense: true,
-                contentPadding: EdgeInsets.all($styles.insets.xs),
-                labelStyle: TextStyle(color: captionColor),
-                hintStyle: TextStyle(color: captionColor.withOpacity(0.5)),
-                prefixStyle: TextStyle(color: captionColor),
-                focusedBorder: OutlineInputBorder(borderSide: BorderSide.none),
-                enabledBorder: UnderlineInputBorder(borderSide: BorderSide.none),
-                hintText: $strings.searchInputHintSearch,
+      child: Row(children: [
+        Gap($styles.insets.xs * 1.5),
+        Icon(Icons.search, color: $styles.colors.caption),
+        Expanded(
+          child: TextField(
+            onSubmitted: onSubmit,
+            controller: textController,
+            focusNode: focusNode,
+            style: TextStyle(color: captionColor),
+            textAlignVertical: TextAlignVertical.top,
+            decoration: InputDecoration(
+              isDense: true,
+              contentPadding: EdgeInsets.all($styles.insets.xs),
+              labelStyle: TextStyle(color: captionColor),
+              hintStyle: TextStyle(color: captionColor.withOpacity(0.5)),
+              prefixStyle: TextStyle(color: captionColor),
+              focusedBorder: OutlineInputBorder(borderSide: BorderSide.none),
+              enabledBorder: UnderlineInputBorder(borderSide: BorderSide.none),
+              hintText: $strings.searchInputHintSearch,
+            ),
+          ),
+        ),
+        Gap($styles.insets.xs),
+        ValueListenableBuilder(
+          valueListenable: textController,
+          builder: (_, value, __) => Visibility(
+            visible: textController.value.text.isNotEmpty,
+            child: Padding(
+              padding: EdgeInsets.only(right: $styles.insets.xs),
+              child: CircleIconBtn(
+                bgColor: $styles.colors.caption,
+                color: $styles.colors.white,
+                icon: AppIcons.close,
+                semanticLabel: $strings.searchInputSemanticClear,
+                size: $styles.insets.lg,
+                iconSize: $styles.insets.sm,
+                onPressed: () {
+                  textController.clear();
+                  onSubmit('');
+                },
               ),
             ),
           ),
-          Gap($styles.insets.xs),
-          ValueListenableBuilder(
-            valueListenable: textController,
-            builder: (_, value, __) => Visibility(
-              visible: textController.value.text.isNotEmpty,
-              child: Padding(
-                padding: EdgeInsets.only(right: $styles.insets.xs),
-                child: CircleIconBtn(
-                  bgColor: $styles.colors.caption,
-                  color: $styles.colors.white,
-                  icon: AppIcons.close,
-                  semanticLabel: $strings.searchInputSemanticClear,
-                  size: $styles.insets.md,
-                  iconSize: $styles.insets.sm,
-                  onPressed: () {
-                    textController.clear();
-                    onSubmit('');
-                  },
-                ),
-              ),
-            ),
-          )
-        ],
-      ),
+        )
+      ]),
     );
   }
 }
