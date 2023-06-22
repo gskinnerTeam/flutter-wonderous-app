eec8c58d88f6629632613519dc7cabb801b6aec9
Shawn <shawn@shawnblais.com>
2023-06-22 13:01:22 -0600

    Update iOS Runner files

---
+++ ios/Wonderous Widget/Wonderous_WidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  Wonderous_WidgetBundle.swift
+//  Wonderous Widget
+//
+//  Created by Shawn on 2023-06-08.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct Wonderous_WidgetBundle: WidgetBundle {
+    var body: some Widget {
+        Wonderous_Widget()
+    }
+}
