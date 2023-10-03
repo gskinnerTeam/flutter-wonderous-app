5065002bce965e9909e9b619d0bacf012a65ef33
Shawn <shawn@gskinner.com>
2023-10-03 14:11:59 -0600

    Update artifact related data classes to use the self-hosted url

--- lib/logic/data/wonders_data/search/search_data.dart
+++ lib/logic/data/wonders_data/search/search_data.dart
@@ -1,17 +1,17 @@
+import 'package:wonders/logic/data/artifact_data.dart';
+
 class SearchData {
-  static const String baseImagePath = 'https://images.metmuseum.org/CRDImages/';
-  static const missingIds = [313256, 327544, 327596, 545776, 38549, 38578, 38473, 38598, 38153, 38203, 64486, 64487];
-  const SearchData(this.year, this.id, this.title, this.keywords, this.imagePath, [this.aspectRatio = 0]);
+  const SearchData(this.year, this.id, this.title, this.keywords, [this.aspectRatio = 0]);
+
   final int year;
   final int id;
-  final String imagePath;
   final String keywords;
   final String title;
   final double aspectRatio;
 
-  String get imageUrl => baseImagePath + imagePath;
+  String get imageUrl => ArtifactData.getSelfHostedImageUrl('$id');
+  String get imageUrlSmall => ArtifactData.getSelfHostedImageUrlSmall('$id');
 
   // used by the search helper tool:
-  String write() =>
-      "SearchData($year, $id, '$title', '$keywords', '$imagePath'${aspectRatio == 0 ? '' : ', ${aspectRatio.toStringAsFixed(2)}'})";
+  String write() => "SearchData($year, $id, '$title', '$keywords')";
 }
