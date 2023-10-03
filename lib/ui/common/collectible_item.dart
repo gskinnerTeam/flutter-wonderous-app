27613c6922bf6dc6d07667e0b4dbebef56e5c8bb
Shawn <shawn@gskinner.com>
2023-10-03 14:10:55 -0600

    Update search data files with fresh items, remove the imageUrl param

--- lib/ui/common/collectible_item.dart
+++ lib/ui/common/collectible_item.dart
@@ -8,7 +8,7 @@ import 'package:wonders/ui/screens/collectible_found/collectible_found_screen.da
 class CollectibleItem extends StatelessWidget with GetItMixin {
   CollectibleItem(this.collectible, {this.size = 64.0, Key? key}) : super(key: key) {
     // pre-fetch the image, so it's ready if we show the collectible found screen.
-    _imageProvider = NetworkImage(AppImage.maybeAddImgProxy(collectible.imageUrl));
+    _imageProvider = NetworkImage(collectible.imageUrl);
     _imageProvider.resolve(ImageConfiguration()).addListener(ImageStreamListener((_, __) {}));
   }
 
