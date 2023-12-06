35333690669794635695f1b05dfda5761e9c02bb
Shawn <shawn@gskinner.com>
2023-12-06 10:34:48 -0700

    Fix map marker placement issue on web

--- lib/ui/common/google_maps_marker.dart
+++ lib/ui/common/google_maps_marker.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:wonders/assets.dart';
 
@@ -6,5 +5,4 @@ Marker getMapsMarker(LatLng position) => Marker(
       markerId: MarkerId('0'),
       position: position,
       icon: AppBitmaps.mapMarker,
-      anchor: Offset(.5, .7),
     );
