bbab1784653a245826c9453e35b7e2d3b4a1dbe2
Shawn <shawn@gskinner.com>
2024-01-11 10:12:40 -0700

    Finish persistent wonderIndex feature

--- lib/logic/settings_logic.dart
+++ lib/logic/settings_logic.dart
@@ -10,8 +10,17 @@ class SettingsLogic with ThrottledSaveLoadMixin {
   late final hasDismissedSearchMessage = ValueNotifier<bool>(false)..addListener(scheduleSave);
   late final isSearchPanelOpen = ValueNotifier<bool>(true)..addListener(scheduleSave);
   late final currentLocale = ValueNotifier<String?>(null)..addListener(scheduleSave);
+  late final prevWonderIndex = ValueNotifier<int?>(null)..addListener(scheduleSave);
 
   final bool useBlurs = !PlatformInfo.isAndroid;
+
+  Future<void> changeLocale(Locale value) async {
+    currentLocale.value = value.languageCode;
+    await localeLogic.loadIfChanged(value);
+    // Re-init controllers that have some cached data that is localized
+    wondersLogic.init();
+    timelineLogic.init();
+  }
 
   @override
   void copyFromJson(Map<String, dynamic> value) {
@@ -19,6 +28,7 @@ class SettingsLogic with ThrottledSaveLoadMixin {
     hasDismissedSearchMessage.value = value['hasDismissedSearchMessage'] ?? false;
     currentLocale.value = value['currentLocale'];
     isSearchPanelOpen.value = value['isSearchPanelOpen'] ?? false;
+    prevWonderIndex.value = value['lastWonderIndex'];
   }
 
   @override
@@ -28,14 +38,7 @@ class SettingsLogic with ThrottledSaveLoadMixin {
       'hasDismissedSearchMessage': hasDismissedSearchMessage.value,
       'currentLocale': currentLocale.value,
       'isSearchPanelOpen': isSearchPanelOpen.value,
+      'lastWonderIndex': prevWonderIndex.value,
     };
-  }
-
-  Future<void> changeLocale(Locale value) async {
-    currentLocale.value = value.languageCode;
-    await localeLogic.loadIfChanged(value);
-    // Re-init controllers that have some cached data that is localized
-    wondersLogic.init();
-    timelineLogic.init();
   }
 }
