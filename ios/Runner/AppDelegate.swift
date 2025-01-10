cdd9f17a281879ae5b9c3da48beed2c5a6be7372
Shawn <shawn@shawnblais.com>
2025-01-10 08:50:39 -0700

    Update iOS build runner files, added min deploy target of iOS 14.0 to satisfy google maps dependency requirements.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,7 +2,7 @@ import UIKit
 import Flutter
 import GoogleMaps
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
