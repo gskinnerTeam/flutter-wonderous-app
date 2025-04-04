7afbd2db13a7624734e2e35ccd7582272a03a483
Alex Garneau <alex@gskinner.com>
2025-04-04 16:42:23 -0600

    Added darker accent3 to deal with existing text contrast issues.

--- lib/styles/colors.dart
+++ lib/styles/colors.dart
@@ -7,6 +7,7 @@ class AppColors {
   /// Common
   final Color accent1 = Color(0xFFE4935D);
   final Color accent2 = Color(0xFFBEABA1);
+  final Color accent3 = Color(0xFFC47642);
   final Color offWhite = Color(0xFFF8ECE5);
   final Color caption = const Color(0xFF7D7873);
   final Color body = const Color(0xFF514F4D);
