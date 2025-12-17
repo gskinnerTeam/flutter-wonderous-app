39364228ace520f5ae7e632bf5f1c7481c5e4f0c
Alex Garneau <alex@gskinner.com>
2025-12-17 11:08:59 -0700

    Fixed improper tabbing.

--- lib/ui/screens/artifact/artifact_carousel/widgets/_bottom_text_content.dart
+++ lib/ui/screens/artifact/artifact_carousel/widgets/_bottom_text_content.dart
@@ -2,12 +2,12 @@ part of '../artifact_carousel_screen.dart';
 
 class _BottomTextContent extends StatelessWidget {
   const _BottomTextContent(
-      {super.key,
-      required this.artifact,
-      required this.height,
-      required this.state,
-      required this.shortMode,
-      required this.overlapMode});
+    {super.key,
+    required this.artifact,
+    required this.height,
+    required this.state,
+    required this.shortMode,
+    required this.overlapMode});
 
   final HighlightData artifact;
   final double height;
@@ -55,14 +55,14 @@ class _BottomTextContent extends StatelessWidget {
                           // Stop text from scaling to make layout a little easier, it's already quite large
                           StaticTextScale(
                             child: overlapMode
-                                ? Container(
-                                    padding: EdgeInsets.all($styles.insets.xxs),
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular($styles.corners.md),
-                                      color: $styles.colors.white.withAlpha(130),
-                                    ),
-                                    child: textContent)
-                                : textContent,
+                              ? Container(
+                                padding: EdgeInsets.all($styles.insets.xxs),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular($styles.corners.md),
+                                  color: $styles.colors.white.withAlpha(130),
+                                ),
+                                child: textContent)
+                              : textContent,
                           ),
                           if (!shortMode) ...[
                             Gap($styles.insets.xxs),
