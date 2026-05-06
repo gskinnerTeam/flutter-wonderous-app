f9c9d2f2712d19d8c4f2fb6a2dd56c545b997f18
Alex Garneau <alex@gskinner.com>
2026-05-06 16:36:26 -0600

    Added try/catch methods to isolate RenderBox error from Collection Screen.

--- lib/ui/common/utils/context_utils.dart
+++ lib/ui/common/utils/context_utils.dart
@@ -3,16 +3,26 @@ import 'package:flutter/material.dart';
 class ContextUtils {
   static Offset? getGlobalPos(BuildContext context, [Offset offset = Offset.zero]) {
     final rb = context.findRenderObject() as RenderBox?;
-    if (rb?.hasSize == true) {
-      return rb?.localToGlobal(offset);
+    try {
+      if (rb?.hasSize == true && rb?.attached == true) {
+        return rb?.localToGlobal(offset);
+      }
+    } catch (e) {
+      // localToGlobal can throw if the render box is in the process of being removed from the tree
+      debugPrint('Error getting global position: $e');
     }
     return null;
   }
 
   static Size? getSize(BuildContext context) {
     final rb = context.findRenderObject() as RenderBox?;
-    if (rb?.hasSize == true) {
-      return rb?.size;
+    try {
+      if (rb?.hasSize == true && rb?.attached == true) {
+        return rb?.size;
+      }
+    } catch (e) {
+      // localToGlobal can throw if the render box is in the process of being removed from the tree
+      debugPrint('Error getting size: $e');
     }
     return null;
   }
