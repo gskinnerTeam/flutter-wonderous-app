f8c6eb998ba0f2192139d2c89dba3761a25b144f
Alex Garneau <alex@gskinner.com>
2026-06-26 11:36:12 -0600

    Trimmed down padding in artifact carousel so it doesn't clip on Android.

--- lib/ui/screens/artifact/artifact_carousel/widgets/_bottom_text_content.dart
+++ lib/ui/screens/artifact/artifact_carousel/widgets/_bottom_text_content.dart
@@ -94,7 +94,7 @@ class _BottomTextContent extends StatelessWidget {
                 expand: true,
                 onPressed: state._handleSearchTap,
               ),
-              if (!shortMode) Gap($styles.insets.lg) else Gap($styles.insets.md),
+              if (!shortMode) Gap($styles.insets.md) else Gap($styles.insets.sm),
             ],
           ),
         ],
